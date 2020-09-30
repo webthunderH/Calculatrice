7074dc22a66a1c6cd0771c3a8709f339aa0d4980
Henry De Donder <Henry.De-Donder-c@transports.gouv.qc.ca>
2020-09-30 16:52:33 -0400

    Add regex expression to find all equation

--- Calculatrice/ExpressionEquation.cs
+++ Calculatrice/ExpressionEquation.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Calculatrice
 {
     public class ExpressionEquation
     {
+        private const char multiplication = '*';
+        private const char division = '/';
+        private const char addition = '+';
+        private const char substraction = '-';
+        private const char modulo = '%';
+        private const char bracketOpening = '(';
+        private const char bracketEnding = ')';
+        private Regex Regex;
+        public static string RegexEquation = @"(?<udouble>\(?\-?\d+,\d+\s+[\+\-\*\%\/]\s+\-?\d+,\d)\)?|(?<uint>\(?\-?\d+\s+[\+\-\*\%\/]\s+\-?\d+)\)?";
+        public Regex myRegex 
+        {
+            get
+            {
+                if (Regex == null)
+                {
+                    Regex = new Regex(RegexEquation);
+                }
+                return Regex;
+            }
+        }
+
         public bool TesterEquation(string stringATester)
         {
-            return true;
+            var lengthTest = 0;
+            Regex.Matches(stringATester).ToList().ForEach(a => lengthTest += a.Value.Length);
+            return lengthTest == stringATester.Length;
         }
 
         public string ExecuteCalculation(string equation)
@@ -22,9 +47,14 @@ namespace Calculatrice
             return 0;
         }
 
-        private List<string> FindAllExpression(string equation)
+        private IEnumerable<string> FindAllExpression(string equations)
         {
-            return new List<string>() { string.Empty };
+            var retour = new List<string>();
+            foreach (var equation in Regex.Split(equations))
+            {
+                retour.Add(equation);
+            }
+            return retour.OrderBy(od => od.Contains(bracketOpening) || od.Contains(bracketEnding)).ThenBy(tb => tb.Contains(multiplication) || tb.Contains(division) || tb.Contains(modulo));
         }
     }
 }
